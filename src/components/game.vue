1c80809ded3dba393bb19bb91caa0cea9f9567a4
zyxa <60773919+zyxa9527@users.noreply.github.com>
2022-09-06 01:44:54 +0800

    Update game.vue

--- src/components/game.vue
+++ src/components/game.vue
@@ -13,7 +13,7 @@
                 id="play"
                 data-modal-toggle="defaultModal"
                 class=" transition  w-24 xl:w-48 bg-white text-sm text-xl text-blue-500 border-4 xl:border-8 border-blue-500 hover:bg-blue-500 hover:text-white font-bold pt-1 xl:py-2 px-4 rounded-full">
-                PLAY1
+                PLAY
             </button>
         </div>
         <div  style="width:100%;height:100%;position:absolute;z-index:5"></div>
@@ -61,7 +61,7 @@
         >
             <div v-if="cell.isLine" :class="cell.lineClass"></div> 
              <template v-if="img">
-                <img :src="img(cell.className)"  alt=""> 
+                <img class="imgclass" :src="img(cell.className)"  alt=""> 
             </template>
         </td>
       </tr>
@@ -222,14 +222,18 @@ export default {
             document.getElementById('wrongAudio').load(); 
         }
         function playmusic() { 
-            document.getElementById('audioBackground').load();
+            document.getElementById('audioBackground').play();
+            // document.getElementById('clickAudio').load(); 
+            // document.getElementById('wrongAudio').load(); 
+        }
+        function imgclass(){
             document.getElementById('clickAudio').load(); 
-            document.getElementById('wrongAudio').load(); 
         }
         //ios沒聲音解法 監聽綁定touchstartHandle事件後呼叫
         document.body.addEventListener('touchstart',loadmusic , false);
         document.body.removeEventListener('touchstart',loadmusic , false); 
         document.getElementById('play').addEventListener('touchend',  playmusic, false);
+        document.getElementsByClassName('imgclass').addEventListener('touchend',  imgclass, false);
         // document.getElementById('play').removeEventListener('touchend',  playmusic, false);
       
     },
